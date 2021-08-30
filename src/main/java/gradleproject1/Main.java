721594ffccbe4bdc84b500e08d0f63b7ace7e11e
Madhav4b1 <madhavarao.nelluri1999@gmail.com>
2021-08-30 13:57:34 -0500

    firstgitcommit

---
+++ src/main/java/gradleproject1/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gradleproject1;
+
+/**
+ *
+ * @author s545525
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+     System.out.println("hello world") ;
+     System.out.println("section 2-- madhav nelluri --success");
+     for(i=0;i<5;i++)
+     {
+         System.out.println(i);
+     }
+     
+     
+     
+     
+// TODO code application logic here
+    }
+    
+}
